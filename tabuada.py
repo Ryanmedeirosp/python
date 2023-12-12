fa441c6471aee0b1933d0ee27f86c5c2698fcfff
Ryanmedeirosp <143143858+Ryanmedeirosp@users.noreply.github.com>
2023-12-12 19:57:04 -0300

    Delete tabuada.py

--- tabuada.py
+++
@@ -1,5 +0,0 @@
-numero = input("Digite um número:\n")
-numero = float(numero)
-print (f"A lista de números:\n{numero}x1={numero*1}\n{numero}x2={numero*2}\n{numero}x3={numero*3}\n{numero}x4={numero*4}\n{numero}x5={numero*5}\n{numero}x6={numero*6}\n{numero}x7={numero*7}\n{numero}x8={numero*8}\n{numero}x9={numero*9}\n{numero}x10={numero*10}\n")
-
-
